d6b57a7c249aa4021e7685ec53faec149b864500
Kevin Johnston <kevinjohnston512@gmail.com>
2015-09-24 19:45:35 -0500

    Added basic setup.py

---
+++ setup.py
@@ -0,0 +1,60 @@
+#!/bin/python3
+'Based on hostname determine which dotfiles should be installed with stow for each passed in user.'
+
+import os
+import subprocess
+from sys import argv
+
+
+##################################################
+############## CONFIGURATION LOGIC ###############
+##################################################
+'Holds all machines'
+def all_machines=set([])
+
+'Creates a new machine and returns it'
+def defineMachine(hostname, dotfilesList):
+  def new_machine = {'hn' : hostname,
+          'dot' : dotfilesList}
+  all_machines.add(new_machine)
+  return new_machine
+
+def default=defineMachine('unknown', ('ssh', 'bash', 'vim', 'X', 'gitconfig', 'gnome2', 'profile', 'selected_editor', 'tmux'))
+
+def dev=defineMachine('aurora', ('ssh', 'bash', 'kde', 'vim', 'X', 'gitconfig', 'gnome2', 'profile', 'selected_editor', 'tmux'))
+def docker_dev=defineMachine('developer', ('ssh', 'bash', 'vim', 'emacs', 'X', 'gitconfig', 'gnome2', 'profile', 'selected_editor', 'tmux'))
+
+##################################################
+################# SETUP LOGIC ####################
+##################################################
+
+
+#check if this is running in a docker environment (because they have dynamic hostnames)
+fil=open("/proc/self/cgroup", "r")
+proc=fil.read()
+fil.close()
+
+proc_list=proc.split("\n")
+for proc in proc_list:
+  if proc.split("/")[1] == "docker":
+    fil=open("/root/hostname", "r")
+    hostname=fil.read()
+    fil.close()
+    
+if hostname == '':
+  hostname=os.uname()[1]
+
+def this_machine={}
+for mach in all_machines:
+  if mach['hn'] == hostname:
+    this_machine = mach
+
+if this_machine == {}:
+  this_machine = default
+
+#for each dotfile to be setup with this machine...
+for dotfil in this_machine['dot']:
+  #...and each user that needs to be setup...
+  for user in sys.argv[1:]:
+    #...call stow 
+    subprocess.call("stow -t/home/" + user + " -d/home/" + user + "dotfiles" + dotfil)
