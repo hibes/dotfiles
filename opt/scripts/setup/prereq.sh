353feeef553ffca2275fd10803e5c1a75afda020
Kevin Johnston <kevinjohnston512@gmail.com>
2017-03-22 00:47:59 -0600

    Update prereq

--- opt/scripts/setup/prereq.sh
+++ opt/scripts/setup/prereq.sh
@@ -4,9 +4,16 @@
 USER="$1"
 
 umask 002
-apt-get install -y git
-apt-get install -y python3
-#mkdir -p /tmp/scripts /opt/scripts /opt/bin; git clone https://github.com/kevinjohnston/scripts.git /tmp/scripts; cd /tmp/scripts; git archive --format=tar -o /tmp/scripts.tar master; tar -x -C /opt/scripts -f /tmp/scripts.tar
-#chmod +x /opt/scripts/*
-#chown -R "$USER:$USER" /opt/bin/
-#chown -R "$USER:$USER" /opt/scripts/
+
+apt-get install -y \
+        git \
+        python3 \
+        wget \
+        curl \
+        telnet \
+        locate \
+        unzip \
+        vim \
+        emacs \
+        hunspell #common spell check program (used by emacs)
+
