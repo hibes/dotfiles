c2949f822f4f8e074d9604e491107e29c51cba6a
Kevin Johnston <kevinjohnston512@gmail.com>
2017-03-29 18:13:16 -0600

    Fix dash compatibility issue

--- opt/scripts/setup/env.sh
+++ opt/scripts/setup/env.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ $(uname) == "Darwin" ]; then
+if [ $(uname) = "Darwin" ]; then
   export OS='mac'
-elif [ $(uname) == "CYGWIN" ]; then 
+elif [ $(uname) = "CYGWIN" ]; then 
   export OS='win'
 else
   export OS='nix'
