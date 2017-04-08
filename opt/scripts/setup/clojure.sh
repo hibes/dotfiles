deade9a570c18f12ef2dfd57a1286a8f0b4ba348
Kevin Johnston <kevin.johnston@dish.com>
2017-04-08 17:03:35 -0600

    Fix lein install error

--- opt/scripts/setup/clojure.sh
+++ opt/scripts/setup/clojure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if ! [ -e "${CUSTOM_PROGRAMS_BINARY_DIR}" ] ; then
+if ! [ -e "${CUSTOM_PROGRAMS_BINARY_DIR}lein" ] ; then
   wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein -q -O ${CUSTOM_PROGRAMS_BINARY_DIR}lein
   chmod +x ${CUSTOM_PROGRAMS_BINARY_DIR}lein
 fi
