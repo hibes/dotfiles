26db5e67fae1c7328f586feb71fb401d47071fb7
Kevin Johnston <kevin.johnston@dish.com>
2017-04-08 15:13:52 -0600

    Add programming mount point to global vars

--- opt/scripts/setup/env.sh
+++ opt/scripts/setup/env.sh
@@ -17,3 +17,17 @@ export CUSTOM_PROGRAMS_DIR=/opt/local/
 export CUSTOM_PROGRAMS_BINARY_DIR=/opt/local/bin/
 export CUSTOM_SCRIPTS_DIR=/opt/scripts/
 export CUSTOM_SETUP_SCRIPTS_DIR=/opt/scripts/setup/
+
+##### Set docker variables
+#######################################################
+case $OS in
+'mac'*)
+  export DOCKER_PROGRAMMING_DIR=/Volumes/programming/
+  ;;
+'win'*)
+  export DOCKER_PROGRAMMING_DIR=/TODO/
+  ;;
+'nix'*)
+  export DOCKER_PROGRAMMING_DIR=/mnt/programming/
+  ;;
+esac
