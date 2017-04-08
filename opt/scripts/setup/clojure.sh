4b7f6044739cb42070c8282cc193263ab4b7be02
Kevin Johnston <kevin.johnston@dish.com>
2017-04-08 17:30:16 -0600

    Hardcode path to env.sh

--- opt/scripts/setup/clojure.sh
+++ opt/scripts/setup/clojure.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+. /opt/scripts/setup/env.sh
+
 if ! [ -e "${CUSTOM_PROGRAMS_BINARY_DIR}lein" ] ; then
   wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein -q -O ${CUSTOM_PROGRAMS_BINARY_DIR}lein
   chmod +x ${CUSTOM_PROGRAMS_BINARY_DIR}lein
