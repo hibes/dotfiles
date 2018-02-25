fedd14e53961c74f5ce0ae1c2545ae10fd97ee66
Kevin Johnston <kevinjohnston512@gmail.com>
2018-02-25 17:19:06 -0600

    Install pip with python2

--- opt/scripts/setup/python2.sh
+++ opt/scripts/setup/python2.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-export PYTHONPATH=.:/usr/local/lib/python2.7/dist-packages/:/usr/lib/python2.7/
-
 # install useful tools
+apt-get install python2
 apt-get install -y python-numpy
+apt-get install pip
 pip install sphnix # documentation generator
 pip install pycontracts # domain driven design
 pip install --upgrade autopep8 # code formatter
+
+export PYTHONPATH=.:/usr/local/lib/python2.7/dist-packages/:/usr/lib/python2.7/
+
