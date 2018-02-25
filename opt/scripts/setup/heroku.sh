c0c76242702ec59902b11138a65509416269311b
Kevin Johnston <kevinjohnston512@gmail.com>
2018-02-25 17:32:04 -0600

    Fix heroku setup

--- opt/scripts/setup/heroku.sh
+++ opt/scripts/setup/heroku.sh
@@ -3,7 +3,7 @@
 # add heroku repo
 add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
 curl -L https://cli-assets.heroku.com/apt/release.key | apt-key add -
-apt-get update
 
 apt-get install -y apt-transport-https
+apt-get update
 apt-get install -y heroku
