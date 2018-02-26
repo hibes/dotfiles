1e4b9103cb60cd46532f97b494a1a7e089686542
Kevin Johnston <kevinjohnston512@gmail.com>
2018-02-25 18:21:30 -0600

    Update python install

--- opt/scripts/setup/python2.sh
+++ opt/scripts/setup/python2.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 # install useful tools
-apt-get install python2
-apt-get install -y python-numpy
-apt-get install pip
+apt-get update
+apt-get install -y python2.7 python-numpy pip
 pip install sphnix # documentation generator
 pip install pycontracts # domain driven design
 pip install --upgrade autopep8 # code formatter
 
 export PYTHONPATH=.:/usr/local/lib/python2.7/dist-packages/:/usr/lib/python2.7/
-
