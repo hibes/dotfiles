c12e534598c42ed53cb36f8f27f00c5372c7eac2
Kevin Johnston <kevinjohnston512@gmail.com>
2018-02-25 19:03:01 -0600

    Fix pip install

--- opt/scripts/setup/python2.sh
+++ opt/scripts/setup/python2.sh
@@ -2,9 +2,10 @@
 
 # install useful tools
 apt-get update
-apt-get install -y python2.7 python-numpy pip
+apt-get install -y python2.7 python-numpy python-pip python-dev build-essential
 pip install sphnix # documentation generator
 pip install pycontracts # domain driven design
 pip install --upgrade autopep8 # code formatter
+
 
 export PYTHONPATH=.:/usr/local/lib/python2.7/dist-packages/:/usr/lib/python2.7/
